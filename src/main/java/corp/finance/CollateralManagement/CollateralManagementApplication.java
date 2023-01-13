8f4477a190202ee50aafd4db16ce5d00112d2e0c
Ravindra Reddy Ambati <ravindra.reddy.ambati@outlook.in>
2023-01-13 18:17:01 +0530

    first commit

---
+++ src/main/java/corp/finance/CollateralManagement/CollateralManagementApplication.java
@@ -0,0 +1,13 @@
+package corp.finance.CollateralManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollateralManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollateralManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/corp/finance/CollateralManagement/CollateralManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package corp.finance.CollateralManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollateralManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
